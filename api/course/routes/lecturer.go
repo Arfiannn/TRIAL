99c1a49538e5290c8ca37d82978e94e2bc14ddde
Ngurah Rio <riyoaditya444@gmail.com>
2025-10-18 16:27:12 +0800

    feat(routes): add lecturer routes for material upload and course view

---
+++ api/course/routes/lecturer.go
@@ -0,0 +1,25 @@
+package routes
+
+import (
+	"course-service/controllers/admin/course"
+	"course-service/controllers/lecturer/material"
+
+	"github.com/gin-gonic/gin"
+)
+
+func LecturerRoutes(r *gin.RouterGroup) {
+
+	materials := r.Group("/materials")
+	{
+		materials.POST("", material.UploadMaterial)
+		materials.GET("", material.GetAllMaterials)
+		materials.GET("/:id", material.GetMaterialByID)
+		materials.PATCH("/:id", material.UpdateMaterial)
+	}
+
+	courses := r.Group("/courses")
+	{
+		courses.GET("", course.GetAllCourses)
+		courses.GET("/:id", course.GetCourseByID)
+	}
+}

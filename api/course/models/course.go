ba5620fdff576d8b2573d139d3af96b3f438e6d6
Ngurah Rio <riyoaditya444@gmail.com>
2025-10-18 23:50:53 +0800

    fix(course): change StartTime and EndTime types to time.Time for better time handling

--- api/course/models/course.go
+++ api/course/models/course.go
@@ -1,17 +1,19 @@
 package models
 
+import "time"
+
 type Course struct {
-	IDCourse    uint   `gorm:"primaryKey;column:id_course"`
-	AdminID     uint   `gorm:"not null;column:adminId"`
-	LecturerID  uint   `gorm:"not null;column:lecturerId"`
-	MajorID     uint   `gorm:"not null;column:majorId"`
-	Semester    string `gorm:"not null;column:semester"`
-	NameCourse  string `gorm:"not null;column:name_course"`
-	Description string `gorm:"not null;column:description"`
-	SKS         int    `gorm:"column:sks"`
-	Day         string `gorm:"column:day"`
-	StartTime   string `gorm:"column:start_time"`
-	EndTime     string `gorm:"column:end_time"`
+	IDCourse    uint      `gorm:"primaryKey;column:id_course"`
+	AdminID     uint      `gorm:"not null;column:adminId"`
+	LecturerID  uint      `gorm:"not null;column:lecturerId"`
+	MajorID     uint      `gorm:"not null;column:majorId"`
+	Semester    string    `gorm:"not null;column:semester"`
+	NameCourse  string    `gorm:"not null;column:name_course"`
+	Description string    `gorm:"not null;column:description"`
+	SKS         int       `gorm:"column:sks"`
+	Day         string    `gorm:"column:day"`
+	StartTime   time.Time `gorm:"column:start_time"`
+	EndTime     time.Time `gorm:"column:end_time"`
 }
 
 func (Course) TableName() string {

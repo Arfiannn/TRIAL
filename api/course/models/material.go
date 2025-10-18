ea4ea5c767645518550d6b638d0eb41b80b8604d
Ngurah Rio <riyoaditya444@gmail.com>
2025-10-18 22:06:27 +0800

    refactor(models): remove Course relation from Material model

--- api/course/models/material.go
+++ api/course/models/material.go
@@ -1,14 +1,12 @@
 package models
 
 type Material struct {
-	IDMaterial   uint   `gorm:"primaryKey;column:id_material"`
-	CourseID     uint   `gorm:"not null;column:courseId"`
-	Title        string `gorm:"not null;column:title"`
-	Description  string `gorm:"column:description"`
-	FileURL      string `gorm:"column:file_url"`
-	CreatedAt string `gorm:"not null;column:created_at"`
-
-	Course Course `gorm:"foreignKey:CourseID;references:IDCourse"`
+	IDMaterial  uint   `gorm:"primaryKey;column:id_material"`
+	CourseID    uint   `gorm:"not null;column:courseId"`
+	Title       string `gorm:"not null;column:title"`
+	Description string `gorm:"column:description"`
+	FileURL     string `gorm:"column:file_url"`
+	CreatedAt   string `gorm:"not null;column:created_at"`
 }
 
 func (Material) TableName() string {

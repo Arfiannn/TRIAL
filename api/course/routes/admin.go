03ac12d9f8467d6295d185138f204a7f50426a2f
Ngurah Rio <riyoaditya444@gmail.com>
2025-10-18 16:26:41 +0800

    feat(routes): add admin routes for course management

---
+++ api/course/routes/admin.go
@@ -0,0 +1,19 @@
+package routes
+
+import (
+	"course-service/controllers/admin/course"
+
+	"github.com/gin-gonic/gin"
+)
+
+func AdminRoutes(r *gin.RouterGroup) {
+
+	courses := r.Group("/courses")
+	{
+		courses.POST("", course.CreateCourse)
+		courses.GET("", course.GetAllCourses)
+		courses.GET("/:id", course.GetCourseByID)
+		courses.PATCH("/:id", course.UpdateCourse)
+		courses.DELETE("/:id", course.DeleteCourse)
+	}
+}

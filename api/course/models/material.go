01f88dd6da47b3f3abdaca2300774c3808348f3a
Ngurah Rio <riyoaditya444@gmail.com>
2025-10-18 23:51:05 +0800

    fix(material): change CreatedAt type to time.Time for better time handling

--- api/course/models/material.go
+++ api/course/models/material.go
@@ -1,12 +1,14 @@
 package models
 
+import "time"
+
 type Material struct {
-	IDMaterial  uint   `gorm:"primaryKey;column:id_material"`
-	CourseID    uint   `gorm:"not null;column:courseId"`
-	Title       string `gorm:"not null;column:title"`
-	Description string `gorm:"column:description"`
-	FileURL     string `gorm:"column:file_url"`
-	CreatedAt   string `gorm:"not null;column:created_at"`
+	IDMaterial  uint      `gorm:"primaryKey;column:id_material"`
+	CourseID    uint      `gorm:"not null;column:courseId"`
+	Title       string    `gorm:"not null;column:title"`
+	Description string    `gorm:"column:description"`
+	FileURL     string    `gorm:"column:file_url"`
+	CreatedAt   time.Time `gorm:"not null;column:created_at"`
 }
 
 func (Material) TableName() string {

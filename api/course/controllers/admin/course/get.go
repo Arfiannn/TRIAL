754e7c94df907db02d9014e52c4c6fe12d0d3735
Ngurah Rio <riyoaditya444@gmail.com>
2025-10-19 16:53:29 +0800

    refactor(course): remove strconv usage and simplify GetCourseByID handler

--- api/course/controllers/admin/course/get.go
+++ api/course/controllers/admin/course/get.go
@@ -4,7 +4,6 @@ import (
 	"course-service/config"
 	"course-service/models"
 	"net/http"
-	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -16,11 +15,7 @@ func GetAllCourses(c *gin.Context) {
 }
 
 func GetCourseByID(c *gin.Context) {
-	id, err := strconv.Atoi(c.Param("id"))
-	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid course ID"})
-		return
-	}
+	id := c.Param("id")
 
 	var course models.Course
 	if err := config.DB.First(&course, "id_course = ?", id).Error; err != nil {

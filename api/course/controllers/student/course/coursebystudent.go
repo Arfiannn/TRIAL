0d4750ffecc10219c3a08d67cdf30c2116dc5036
Ngurah Rio <riyoaditya444@gmail.com>
2025-10-21 00:26:08 +0800

    feat(course): add GetCoursesByStudent to fetch courses by semester, faculty, and major

---
+++ api/course/controllers/student/course/coursebystudent.go
@@ -0,0 +1,42 @@
+package course
+
+import (
+	"course-service/config"
+	"course-service/models"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func GetCoursesByStudent(c *gin.Context) {
+	studentID := c.Query("studentId")
+
+	if studentID == "" {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "studentId tidak boleh kosong"})
+		return
+	}
+
+	var student models.User
+	if err := config.DB.First(&student, "id_user = ?", studentID).Error; err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Mahasiswa tidak ditemukan"})
+		return
+	}
+
+	var courses []models.Course
+	if err := config.DB.
+		Where("semester = ? AND facultyId = ? AND majorId = ?", student.Semester, student.FacultyID, student.MajorID).
+		Find(&courses).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Gagal mengambil data course"})
+		return
+	}
+
+	if len(courses) == 0 {
+		c.JSON(http.StatusNotFound, gin.H{"message": "Tidak ada course yang cocok untuk mahasiswa ini"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Berhasil mengambil data course sesuai semester, fakultas, dan jurusan mahasiswa",
+		"data":    courses,
+	})
+}

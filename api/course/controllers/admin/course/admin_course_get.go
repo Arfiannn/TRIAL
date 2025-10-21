adb070156df7959e11b20eb1003725a311d72225
Ngurah Rio <riyoaditya444@gmail.com>
2025-10-21 15:37:51 +0800

    fix(course): update GetAllCourses and GetCourseByID handlers to use JWT authorization

--- api/course/controllers/admin/course/admin_course_get.go
+++ api/course/controllers/admin/course/admin_course_get.go
@@ -9,17 +9,33 @@ import (
 )
 
 func GetAllCourses(c *gin.Context) {
+	roleID := c.GetUint("role_id")
+	if roleID != 1 {
+		c.JSON(http.StatusForbidden, gin.H{"error": "Hanya admin yang boleh mengakses data ini"})
+		return
+	}
+
 	var courses []models.Course
-	config.DB.Find(&courses)
+	if err := config.DB.Find(&courses).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Gagal mengambil data mata kuliah"})
+		return
+	}
+
 	c.JSON(http.StatusOK, gin.H{"data": courses})
 }
 
 func GetCourseByID(c *gin.Context) {
+	roleID := c.GetUint("role_id")
+	if roleID != 1 {
+		c.JSON(http.StatusForbidden, gin.H{"error": "Hanya admin yang boleh mengakses data ini"})
+		return
+	}
+
 	id := c.Param("id")
 
 	var course models.Course
 	if err := config.DB.First(&course, "id_course = ?", id).Error; err != nil {
-		c.JSON(http.StatusNotFound, gin.H{"error": "Course not found"})
+		c.JSON(http.StatusNotFound, gin.H{"error": "Mata kuliah tidak ditemukan"})
 		return
 	}
 

6ee91560f52e88a0ebb3fe5cecc368f01e2dd5d8
Ngurah Rio <riyoaditya444@gmail.com>
2025-10-21 17:07:41 +0800

    feat(cors): add CORS middleware configuration to the router

--- api/course/routes/routes.go
+++ api/course/routes/routes.go
@@ -1,10 +1,25 @@
 package routes
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"time"
+)
 
 func SetupRouter() *gin.Engine {
 	r := gin.Default()
+
+	r.Use(cors.New(cors.Config{
+		AllowAllOrigins:  true,
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: false,
+		MaxAge:           12 * time.Hour,
+	}))
+
 	SetupRoutes(r)
+
 	return r
 }
 

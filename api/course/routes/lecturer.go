82114bf1bf8349b5705833056b211637dc64e627
Ngurah Rio <riyoaditya444@gmail.com>
2025-10-21 16:57:36 +0800

    refactor(routes): move JWT authorization middleware to lecture route group

--- api/course/routes/lecturer.go
+++ api/course/routes/lecturer.go
@@ -3,18 +3,22 @@ package routes
 import (
 	"course-service/controllers/lecturer/course"
 	"course-service/controllers/lecturer/material"
+	"course-service/middleware"
 
 	"github.com/gin-gonic/gin"
 )
 
 func LecturerRoutes(r *gin.RouterGroup) {
+	lecturer := r.Group("")
+	lecturer.Use(middleware.JWTAuthMiddleware())
 
 	materials := r.Group("/materials")
 	{
 		materials.POST("", material.UploadMaterial)
 		materials.GET("", material.GetAllMaterials)
 		materials.GET("/:id", material.GetMaterialByID)
-		materials.PATCH("/:id", material.UpdateMaterial)
+		materials.PUT("/:id", material.UpdateMaterial)
+		materials.DELETE("/:id", material.DeleteMaterial)
 	}
 
 	courses := r.Group("/courses")

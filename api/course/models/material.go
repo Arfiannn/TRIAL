5bae1010dfa5af0ef4834c26a923318893585ccb
Ngurah Rio <riyoaditya444@gmail.com>
2025-10-22 22:59:02 +0800

    fix(material): change FileURL type from string to []byte and type FileType

--- api/course/models/material.go
+++ api/course/models/material.go
@@ -7,7 +7,8 @@ type Material struct {
 	CourseID    uint      `gorm:"not null;column:courseId" json:"courseId"`
 	Title       string    `gorm:"not null;column:title" json:"title"`
 	Description string    `gorm:"column:description" json:"description"`
-	FileURL     string    `gorm:"column:file_url" json:"file_url"`
+	FileURL     []byte    `gorm:"column:file_url" json:"-"`
+	FileType    string    `gorm:"column:file_type" json:"file_type"`
 	CreatedAt   time.Time `gorm:"not null;column:created_at" json:"created_at"`
 }
 

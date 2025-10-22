d50f6e05cfceda9ee1efbab2fb5e698b5f5c367c
Ngurah Rio <riyoaditya444@gmail.com>
2025-10-22 18:41:58 +0800

    fix(models): add missing JSON tag in Material type

--- api/course/models/material.go
+++ api/course/models/material.go
@@ -3,12 +3,12 @@ package models
 import "time"
 
 type Material struct {
-	IDMaterial  uint      `gorm:"primaryKey;column:id_material"`
-	CourseID    uint      `gorm:"not null;column:courseId"`
-	Title       string    `gorm:"not null;column:title"`
-	Description string    `gorm:"column:description"`
-	FileURL     string    `gorm:"column:file_url"`
-	CreatedAt   time.Time `gorm:"not null;column:created_at"`
+	IDMaterial  uint      `gorm:"primaryKey;column:id_material" json:"id_material"`
+	CourseID    uint      `gorm:"not null;column:courseId" json:"courseId"`
+	Title       string    `gorm:"not null;column:title" json:"title"`
+	Description string    `gorm:"column:description" json:"description"`
+	FileURL     string    `gorm:"column:file_url" json:"file_url"`
+	CreatedAt   time.Time `gorm:"not null;column:created_at" json:"created_at"`
 }
 
 func (Material) TableName() string {

627ba6a39567295b8b1b9412844ee050eee86e62
Arfiannn <arfianpndg@gmail.com>
2025-10-25 04:05:31 +0800

    feat: streamline JWT secret configuration and enhance assignment creation logic

--- api/operational/models/course.go
+++ api/operational/models/course.go
@@ -1,23 +1,19 @@
 package models
 
-import (
-	"time"
-
-	"gorm.io/gorm"
-)
+import "gorm.io/gorm"
 
 type Course struct {
 	IDCourse    uint           `gorm:"primaryKey;column:id_course" json:"id_course"`
 	AdminID     uint           `gorm:"not null;column:adminId" json:"adminId"`
 	LecturerID  uint           `gorm:"not null;column:lecturerId" json:"lecturerId"`
 	MajorID     uint           `gorm:"not null;column:majorId" json:"majorId"`
-	Semester    string         `gorm:"not null;column:semester" json:"semester"`
+	Semester    int            `gorm:"not null;column:semester" json:"semester"`
 	NameCourse  string         `gorm:"not null;column:name_course" json:"name_course"`
 	Description string         `gorm:"not null;column:description" json:"description"`
 	SKS         int            `gorm:"column:sks" json:"sks"`
 	Day         string         `gorm:"column:day" json:"day"`
-	StartTime   time.Time      `gorm:"column:start_time" json:"start_time"`
-	EndTime     time.Time      `gorm:"column:end_time" json:"end_time"`
+	StartTime   string         `gorm:"column:start_time" json:"start_time"`
+	EndTime     string         `gorm:"column:end_time" json:"end_time"`
 	DeletedAt   gorm.DeletedAt `gorm:"column:deleted_at" json:"deleted_at"`
 }
 

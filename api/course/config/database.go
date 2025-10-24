af38b9e0fe379d72e0b44472ea2c2f22177bef56
Ngurah Rio <riyoaditya444@gmail.com>
2025-10-24 23:10:01 +0800

    refactor(config): make .env loading optional and add success log message

--- api/course/config/database.go
+++ api/course/config/database.go
@@ -13,10 +13,7 @@ import (
 var DB *gorm.DB
 
 func ConnectDB() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
+	_ = godotenv.Load()
 
 	dsn := fmt.Sprintf(
 		"%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
@@ -27,9 +24,11 @@ func ConnectDB() {
 		os.Getenv("DB_NAME"),
 	)
 
+	var err error
 	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
-		log.Fatal("Database connection error: ", err)
+		log.Fatal("❌ Database connection error: ", err)
 	}
 
+	fmt.Println("✅ Database connected successfully")
 }
